ed73d401352a91f657a7efb837d2ad38dacbe8e9
waverode <waverode@outlook.com>
2020-05-12 21:44:04 +0800

    0.0.1

--- travel/src/main.js
+++ travel/src/main.js
@@ -3,6 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/styles/iconfont.css'
+// 解决移动端css不统一的问题
+import './assets/styles/reset.css'
+// 解决移动端1像素框的问题
+import './assets/styles/border.css'
+// 解决移动端点击会延迟300ms的问题，安装fastclick库
+// import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
 
---
+++ travel/src/pages/home/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class='header'>
+    <div class='header-left'>
+      <span class="iconfont backicon">&#xe600;</span>
+    </div>
+    <div class='header-input'>
+      <span class="iconfont">&#xe633;</span>
+      输入城市、景点、游玩主题</div>
+    <div class='header-right'>城市
+      <span class="iconfont downicon">&#xe601;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import'~styles/varibles.styl'
+  .header
+    line-height: 0.86rem
+    display: flex
+    background-color: $bgColor
+    color: #FFFFFF
+    .header-left
+      width: .64rem
+      float: left
+      .backicon
+        display : block
+        text-align : center
+        font-size : .4rem
+    .header-input
+      flex: 1
+      line-height : .64rem
+      background-color : #FFFFFF
+      color: gray
+      border-radius : .1rem
+      margin-top : .12rem
+      padding-left : .2rem
+      height : .64rem
+      margin-left : .2rem
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align : center
+      .downicon
+        font-size : .24rem
+
+</style>
