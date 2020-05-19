8fae876ff33ff74517a85e748a5946a3ac14d7d6
waverode <waverode@outlook.com>
2020-05-19 14:36:49 +0800

    finish city model

--- travel/src/pages/city/City.vue
+++ travel/src/pages/city/City.vue
@@ -1,18 +1,28 @@
 <template>
     <main>
         <city-header></city-header>
+        <city-search></city-search>
+        <city-list></city-list>
+        <city-alphabet></city-alphabet>
     </main>
 </template>
 
 <script>
 import CityHeader from './components/Header.vue'
+import CitySearch from './components/Search.vue'
+import CityList from './components/List.vue'
+import CityAlphabet from './components/Alphabet.vue'
 export default{
   name: 'City',
   components: {
-    CityHeader
+    CityHeader,
+    CitySearch,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
 
 <style>
+
 </style>
